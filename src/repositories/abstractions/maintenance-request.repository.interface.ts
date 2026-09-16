2ba98e77cca3dff525cccae0cf393b691ee3be8b
KBDRT <midaskbadrat@gmail.com>
2026-09-16 13:58:05 +0300

    feat: Добавлен сервис и репозитории для сущности

---
+++ src/repositories/abstractions/maintenance-request.repository.interface.ts
@@ -0,0 +1,10 @@
+import { MaintenanceRequest } from "../../domains/entities/maintenance-request.entity.js";
+import { getEquipmentsRequest } from "../../dto/contracts/get-equipments.request.js";
+
+export interface IMaintenanceRequestRepository {
+  get(request: getEquipmentsRequest): Promise<MaintenanceRequest[]>,
+  add(newRequest: MaintenanceRequest): Promise<string>,
+  getById(id: string): Promise<MaintenanceRequest | undefined>;
+  update(updatedRequest: MaintenanceRequest): Promise<void>;
+  delete(id: string): Promise<void>;
+}
---
+++ src/services/maintenance-request.service.ts
@@ -0,0 +1,74 @@
+import { Equipment } from "../domains/entities/equipment.entity.js";
+import { MaintenanceRequest } from "../domains/entities/maintenance-request.entity.js";
+import { CreateEquipmentRequest } from "../dto/contracts/create-equipment.request.js";
+import { GetEquipmentWeatherResponse } from "../dto/contracts/get-equipment-weather.response.js";
+import { getEquipmentsRequest } from "../dto/contracts/get-equipments.request.js";
+import { UpdateEquipmentRequest } from "../dto/contracts/update-equipment.request.js";
+import { WeatherRule } from "../dto/weather-rule.js";
+import { IEquipmentRepository } from "../repositories/abstractions/equipment-repository.interface.js";
+import { EquipmentRepositoryMemory } from "../repositories/implementations/in-memory-equipment-repository.js";
+import { weatherSuitableSchema } from "../validators/schemas/equipment/weather-suitable.schema.js";
+import { getWeatherAsync } from "./weather.service.js";
+
+// const repository: IEquipmentRepository = new EquipmentRepositoryMemory();
+
+//todo :rewrite errors
+export const addEquipment = async(equipmentInfo: CreateEquipmentRequest): Promise<string> => {
+
+  return "1";
+  // const installedDate = new Date(equipmentInfo.installedAt);
+  // const currentDate = new Date();
+  // if (installedDate > currentDate) {
+  //   throw new Error("Дата в будущем");
+  // }
+
+  // const existing = await repository.getBySerialNumber(equipmentInfo.serialNumber);
+  // if (existing) {
+  //   throw new Error("Сериал номер уже есть");
+  // }
+  
+  // const equipment = Equipment.create(equipmentInfo);
+  // await repository.add(equipment);
+  // return equipment.id;
+};
+
+export const getEquipments = async(request: getEquipmentsRequest): Promise<MaintenanceRequest[]> => {
+  // return await repository.get(request);
+  return [];
+};
+
+export const deleteEquipment = async(id: string): Promise<void> => {
+  // const existing = await repository.getById(id);
+  // if (!existing) {
+  //   throw new Error("Не найден");
+  // }
+
+  // await repository.delete(id);
+};
+
+export const getEquipment = async(id: string): Promise<MaintenanceRequest | undefined> => {
+  // const existing = await repository.getById(id);
+  // if (!existing) {
+  //   throw new Error("Не найден");
+  // }
+
+  // return await repository.getById(id);
+  return undefined;
+};
+
+export const updateEquipment = async(id: string, equipmentInfo: UpdateEquipmentRequest): Promise<void> => {
+  // const equipment = await repository.getById(id);
+  // if (!equipment) {
+  //   throw new Error("Не найден");
+  // }
+
+  // if (equipment) {
+  //   const updated = { ...equipment, ...equipmentInfo };
+  //   await repository.update(updated);
+  // }
+};
+
+
+export const updateRequestStatus = async(id: string): Promise<void> => {
+
+}
