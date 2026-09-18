1cb3cc69ec5e6ebadb70496387bb671704c718ba
KBDRT <midaskbadrat@gmail.com>
2026-09-18 10:51:13 +0300

    feat: Добавлено завершение процесса для необработанных ситуаций

--- src/lib/context.ts
+++ src/lib/context.ts
@@ -1,11 +1,19 @@
 import { AsyncLocalStorage } from 'node:async_hooks';
 import { NextFunction, Request, Response } from "express";
 import { logger } from './pino.js';
+import { ReqId } from 'pino-http';
+import { BaseLogger } from 'pino';
 
-export const store = new AsyncLocalStorage();
+interface StoreInfo{
+  reqId: ReqId;
+  log: BaseLogger
+}
+
+
+export const store = new AsyncLocalStorage<StoreInfo>();
 
 export const contextMiddleware = (req: Request, res: Response, next: NextFunction) => {
   store.run({ reqId: req.id, log: req.log }, next);
 };
 
-export const getLog = () => store.getStore() ?? logger
+export const getLog = () => store.getStore()?.log ?? logger
--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,14 @@
 import appConfig from "./config/app.config.js";
 import app from "./app.js";
+import { getLog } from "./lib/context.js";
 
-app.listen(appConfig.port);
+const server = app.listen(appConfig.port);
+
+function shutdown(reason: string, err: unknown) {
+  getLog().fatal({ err, reason }, 'shutting down');
+  server.close(() => process.exit(1));
+  setTimeout(() => process.exit(1), 10_000).unref();
+}
+
+process.on('uncaughtException', (err) => shutdown('uncaughtException', err));
+process.on('unhandledRejection', (err) => shutdown('unhandledRejection', err));
