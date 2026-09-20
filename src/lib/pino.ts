c5f2ccbee528eaa9f252294b7e899d5dac0d2824
KBDRT <midaskbadrat@gmail.com>
2026-09-20 12:23:29 +0300

    fix: Добавлен лог для старта и исправлен импорт

--- src/lib/pino.ts
+++ src/lib/pino.ts
@@ -1,11 +1,11 @@
+import appConfig from '../config/app.config.js';
 import pino from 'pino';
 import NODE_ENV_VALUES from '../config/node_env.enum.js';
-import appConfig from '../config/app.config.js';
 
 export const logger = pino({
-  level: appConfig.logLevel ?? 'info',
+  level: process.env.LOG_LEVEL ?? 'info',
   redact: ['req.headers.authorization', 'req.headers.cookie', '*.password', '*.token'],
-  ...(appConfig.nodeEnv != NODE_ENV_VALUES.PRODUCTION && {
+  ...(process.env.NODE_ENV != NODE_ENV_VALUES.PRODUCTION && {
     transport: { target: 'pino-pretty', options: {singleLine: true} },
   }),
   
