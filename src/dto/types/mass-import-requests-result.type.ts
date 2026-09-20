e01bbd39977c52ef120220f9cc569da30b3c4ec7
KBDRT <midaskbadrat@gmail.com>
2026-09-20 12:06:01 +0300

    fix: Исправлены импорты для билда

--- src/dto/types/mass-import-requests-result.type.ts
+++ src/dto/types/mass-import-requests-result.type.ts
@@ -1,6 +1,4 @@
-import { MaintenanceRequest } from "../../domains/entities/maintenance-request.entity.js";
 import { CreateMaintenanceRequestMassDto } from "../maintenance-request/create-maintenance-request-mass.dto.js";
-import { CreateMaintenanceRequestDto } from './../maintenance-request/create-maintenance-request.dto';
 
 export interface MassImportRequestsResult{
   imports: CreateMaintenanceRequestMassDto[],
--- src/lib/pino.ts
+++ src/lib/pino.ts
@@ -1,6 +1,6 @@
 import pino from 'pino';
-import NODE_ENV_VALUES from '../config/node_env.enum';
-import appConfig from '../config/app.config';
+import NODE_ENV_VALUES from '../config/node_env.enum.js';
+import appConfig from '../config/app.config.js';
 
 export const logger = pino({
   level: appConfig.logLevel ?? 'info',
